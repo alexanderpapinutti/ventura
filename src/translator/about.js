115cca0fb23780bd9e6c3332450cbbd05e6c6a30
alexpapinutti <alexpapinutti@gmail.com>
2021-10-04 17:58:43 +0200

    Removed unused assets

--- src/translator/about.js
+++ src/translator/about.js
@@ -14,14 +14,14 @@ export const about = {
   },
   description: {
     en:
-      'Let me tell you more about myself. My name is Alexander Papinutti and I am a Full Stack web developer specialized in Javascript and I ' +
+      'Let me tell you more about myself. My name is Alexander Papinutti and I am a Full Stack web developer specialized in Javascript. I ' +
       'started my journey as a professional in 2019, where I worked for a company that offered software for fleet management. My experience there allowed me to to build ' +
       'a solid knowledge base and confidence in my ability to provide a service of production and maintenance of software. I offer my services as a Freelance Web Developer.',
     it:
       'Lascia che ti dica qualcosa di più su di me. Il mio nome è Alexander Papinutti e sono uno sviluppatore web Full Stack specializzato in Javascript. Ho ' +
       "iniziato il mio percorso professionale nel 2019, lavorando per una società che offre software per amministrazione di flotte di veicoli. L'esperienza matturata mi ha permesso di costruire " +
       'una solida base di conoscenze e fiducia nella mia capacità per fornire un servizio di produzzione e manutenzione di software. Offro le mie prestazioni professionali' +
-      ' in qualità di Svilupatore Web come libero professionista.',
+      ' in qualità di Sviluppatore Web come libero professionista.',
   },
   experience: {
     professional: {
